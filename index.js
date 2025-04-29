4fb50a121cd4ec5b097b0ff9feab3d9d9ce332ed
RafaellaaSilva <167482716+RafaellaaSilva@users.noreply.github.com>
2025-04-29 20:41:08 -0300

    Estrutuaçaõ e exibição das mensagens

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const http = require('http');
+const socketIO = require('socket.io')
 
 const app = express();
 
 const server = http.createServer(app);
 
+const io = socketIO(server);
+
 const ejs = require ('ejs');
-const path = require('patch');
+const path = require('path');
+const { Socket } = require('dgram');
 
 app.use(express.static(path.join(__dirname, 'public')));
 //console.log(path.join(__dirname, 'public'));
@@ -17,6 +21,33 @@ app.engine('html', ejs.renderFile);
 
 app.use('/', (request, response) => {
     response.render('index.html')
+});
+
+// LOGICA DO SOCKET.IO - ENVIO E PROPAGAÇÃO DE MENSAGEM //
+
+//Array que simula o banco de dados das mensagnes
+
+let messages = [];
+// ESTRUTURA DE CONEXÃO DO SCKET.IO
+
+io.on('connection', socket => {
+
+    //TESTE DE CONEXÃO
+    console.log('NOVO USUARIO CONECTADO: ' + socket.id);
+
+
+    // Recupera e mantem(mostra) as mensagens entre o from e o back 
+    socket.emit('previousMessage', messages);
+
+    //logica de chat chat quando uma mensagem é enviada
+    socket.on('sedMessage', data => {
+
+        //adicionar a mensagem no final do array de messagem 
+        messages.push(data)
+
+        socket.broadcast.emit('receiveMessage', data)
+    });
+
 });
 
 server.listen(3000, () => {
