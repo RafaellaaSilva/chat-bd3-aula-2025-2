1d0a61866c2e538cf645e7b2ce3ea0f727d0919c
RafaellaaSilva <167482716+RafaellaaSilva@users.noreply.github.com>
2025-05-13 20:20:24 -0300

    Salvando as mensagens no Banco MongoDB

--- index.js
+++ index.js
@@ -1,55 +1,85 @@
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io')
+const express = require("express");
+const http = require("http");
 
 const app = express();
 
-const server = http.createServer(app);
+const server = http.createServer(app);  
 
-const io = socketIO(server);
-
-const ejs = require ('ejs');
+const ejs = require("ejs");
 const path = require('path');
-const { Socket } = require('dgram');
+const socetIO = require('socket.io');
+const { Socket } = require("dgram");
 
-app.use(express.static(path.join(__dirname, 'public')));
-//console.log(path.join(__dirname, 'public'));
+const io = socetIO(server)
+
+const mongoose = require('mongoose')
+
+app.use(express.static(path.join(__dirname,'public')));
 
 app.set('view', path.join(__dirname, 'public'));
 
 app.engine('html', ejs.renderFile);
 
-app.use('/', (request, response) => {
-    response.render('index.html')
-});
+app.use('/', (req,res)=>{
+    res.render('indes.html')
+})
 
-// LOGICA DO SOCKET.IO - ENVIO E PROPAGAÇÃO DE MENSAGEM //
+function connectDB(){
+    let dbURL = 'mongodb+srv://rafaelladrive1:R6W2OZu8ZoLub3pS@cluster0.jupxc.mongodb.net/'
+    mongoose.connect(dbURL)
+    mongoose.connection.on('error', console.error.bind(console, 'Connection error:'))
+    mongoose.connection.once('open', function(){
+        console.log('ATLAS MONGODB CONECTADO COM SUCESSO!')
+    })
+}
 
-//Array que simula o banco de dados das mensagnes
+connectDB()
 
+let Message = mongoose.model('Message', {usuario:String, data_hora:String, message:String})
+
+
+// LÓGICA DO SOCKET.IO - ENVIO DE PROPAGAÇÃO DE MENSAGENS
+
+//array que simula o branco de dados:
 let messages = [];
-// ESTRUTURA DE CONEXÃO DO SCKET.IO
 
-io.on('connection', socket => {
-
-    //TESTE DE CONEXÃO
-    console.log('NOVO USUARIO CONECTADO: ' + socket.id);
-
-
-    // Recupera e mantem(mostra) as mensagens entre o from e o back 
-    socket.emit('previousMessage', messages);
-
-    //logica de chat chat quando uma mensagem é enviada
-    socket.on('sedMessage', data => {
-
-        //adicionar a mensagem no final do array de messagem 
-        messages.push(data)
-
-        socket.broadcast.emit('receiveMessage', data)
+Message.find({})
+    .then(docs=>{
+        messages = docs 
+    }).catch(error=>{
+        console.log(error);
     });
 
-});
+//ESTRUTURA DE CONEXÃO DO SOCKET.IO
+io.on('connection', socket=>{
 
-server.listen(3000, () => {
-    console.log('CHAT RODANDO EM - HTTP://localhost:3000')
-});
+    //teste de conexão
+    console.log('NOVO USUÁRIO CONECTADO: ' + socket.id)
+
+    //Recupera e mantém (exibe) as mensagens entre o front e o back:
+    socket.emit('previousMessage', messages);
+
+    //Lógica de chat quando uma mensagem é enviada:
+    socket.on('sendMessage', data=>{
+
+        //adiciona a mensagem no ginal do array de menssagens:
+        //messages.push(data);
+
+        let message = new Message(data)
+
+        message.save()
+        .then(
+            socket.broadcast.emit('receivedMessage', data)
+        ).catch(error=>{
+                console.log(error)
+            });
+
+        console.log('QTD MENSAGENS: '+messages.length)
+
+    });
+
+    console.log('QTD MENSAGENS: '+messages.length)
+
+})
+
+server.listen(3000, ()=>{console.log("chat rodando em http://localhost:3000")});
